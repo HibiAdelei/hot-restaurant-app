fc526cbd36aadb62662acef1a3df927e1c9c6e55
HibiAdelei <lilyadelame@gmail.com>
2021-07-14 14:19:09 -0700

    apiroute done and small bugfix

---
+++ appdata/waitlistdata.js
@@ -0,0 +1,5 @@
+// holds data for waitlisted tables, initialised as an empty array
+var waitListArray = [];
+
+//exports array
+module.exports = waitingArray;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,31 @@
-var tableData = require("../appdata/tabledata.js");
-var waitListData = require("../data/waitlistdata");
+var tableData = require("../appdata/tabledata");
+var waitListData = require("../appdata/waitlistdata");
 
 
 
 module.exports = function(app) {
+
+
+    // get requests should return table data and waitlist data
+    app.get("/api/tables", function(req, res) {
+        res.json(tableData);
+      });
+    
+      app.get("/api/waitlist", function(req, res) {
+        res.json(waitListData);
+      });
+
+      app.post("/api/tables", function(req, res) {
+       
+        if (tableData.length < 5) {
+          tableData.push(req.body);
+          res.json(true);
+        }
+        else {
+          waitListData.push(req.body);
+          res.json(false);
+        }
+      });
+
+
+};
