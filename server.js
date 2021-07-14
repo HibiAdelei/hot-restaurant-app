d6d981caee85a0915f57190dfdb7dcc965a5f5f4
HibiAdelei <lilyadelame@gmail.com>
2021-07-14 13:10:13 -0700

    initial server code

---
+++ server.js
@@ -0,0 +1,16 @@
+// require express
+var express = require("express");
+var app = express();
+
+// initial port, listener at bottom
+var PORT = process.env.PORT || 3000;
+
+// routers 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//require apiroute
+//require html route 
+app.listen(port, function() {
+ console.log("Application listening on port " + PORT);
+});
