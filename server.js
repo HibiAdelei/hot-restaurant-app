828a4258d6d260b19d414867a899feafa15f4841
wooglow <74934952+wooglow@users.noreply.github.com>
2021-07-14 14:11:46 -0700

    Routes are made, .gitignore added

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ var express = require("express");
 var app = express();
 const path = require('path');
 
-
 // initial port, listener at bottom
 var PORT = process.env.PORT || 3000;
+
+const reservation = 
+[
+  {
+    "Name": "Owner",
+    "Phone": 1111111111,
+    "Email": "aaa@bbb.com",
+    "Id": 1123
+  },
+]
 
 // routers 
 app.use(express.urlencoded({ extended: true }));
@@ -18,8 +27,14 @@ app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, './html/rese
 
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, './html/tables.html')));
 
-app.get('/api/tables', (req, res) => {
-    res.sendFile(path.join(__dirname, './appdata/table.json'))
+app.get('/api/tables', (req, res) => res.json(reservation));
+
+app.post('/api/tables', (req, res) => {
+    const newReservation = req.body;
+    // For Checking
+    console.log(newReservation);
+    reservation.push(newReservation);
+    res.json(newReservation);
 });
 //require html route 
 app.listen(PORT, function() {
